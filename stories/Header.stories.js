624470d90e14d826880e10fc971e7d64dfbfe07b
Eduardo Goes <e.goes@unifesp.br>
2022-11-24 15:22:29 -0300

    ✨ start writing Header stories

---
+++ stories/Header.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header } from '../components/Header';
+
+export default {
+    title: 'Header',
+    component: Header
+}
+
+export const Primary = () => {
+    return <Header user={"Edu"} isAuthenticated="false"></Header>
+}
